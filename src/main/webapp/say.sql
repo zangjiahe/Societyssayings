5f35b003e7ce6d86db73c422b1eb96e5b099765d
1314aijiahe <2210074929@qq.com>
2020-02-10 00:35:02 +0800

    数据库 已收部分语录

---
+++ src/main/webapp/say.sql
@@ -0,0 +1,258 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地数据库
+Source Server Version : 50533
+Source Host           : localhost:3306
+Source Database       : say
+
+Target Server Type    : MYSQL
+Target Server Version : 50533
+File Encoding         : 65001
+
+Date: 2020-02-10 00:34:16
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for say
+-- ----------------------------
+DROP TABLE IF EXISTS `say`;
+CREATE TABLE `say` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `say` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=229 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of say
+-- ----------------------------
+INSERT INTO `say` VALUES ('1', '\r\n花有百般红，人与狗不同。');
+INSERT INTO `say` VALUES ('2', '\r\n我可以笑着给你一个台阶下，也可以反手给你一个大嘴巴。');
+INSERT INTO `say` VALUES ('3', '\r\n要想生活过得去，头上总得带点绿。');
+INSERT INTO `say` VALUES ('4', '\r\n跟着大哥混,三天饿九顿,偶尔还要挨钢棍。');
+INSERT INTO `say` VALUES ('5', '\r\n月亮是我抛的硬币，两面都是梦见你。');
+INSERT INTO `say` VALUES ('6', '\r\n好好生活慢慢相遇。');
+INSERT INTO `say` VALUES ('7', '\r\n寒王送你们一句话，人在做天在看，坏事别瞎干 ，谁说我的语言太乱，我字字句句都在劝人行善。');
+INSERT INTO `say` VALUES ('8', '\r\n思君如满月，夜夜减清辉。');
+INSERT INTO `say` VALUES ('9', '\r\n翻江龙遇下山虎，社会不由你做主');
+INSERT INTO `say` VALUES ('10', '\r\n有空一起谈个恋爱，没空我继续暗恋你。');
+INSERT INTO `say` VALUES ('11', '\r\n精神语录网上抄，小心钢棍身上飘。');
+INSERT INTO `say` VALUES ('12', '\r\n于我而言，你是最好的。');
+INSERT INTO `say` VALUES ('13', '\r\n世事无常，你是人间琳琅。');
+INSERT INTO `say` VALUES ('14', '\r\n灯红酒绿惹人醉，摇头小伙不会累。');
+INSERT INTO `say` VALUES ('15', '\r\n你逆光而来配得上这世间所有的好。');
+INSERT INTO `say` VALUES ('16', '\r\n名声在外，有好有坏，以前是以前，现在是现在，我就站在你面前，你看我几分像从前。');
+INSERT INTO `say` VALUES ('17', '\r\n你没拥有的人脉叫我姐妹，你得不到的男人叫我宝贝。');
+INSERT INTO `say` VALUES ('18', '\r\n只要小伙精神在，到哪都是实力派。');
+INSERT INTO `say` VALUES ('19', '\r\n都是伊利优酸乳，装什么蒙牛特伦舒。');
+INSERT INTO `say` VALUES ('20', '\r\n万事浮沉，你是人间归途。');
+INSERT INTO `say` VALUES ('21', '\r\n做人不要太攀比，要比就比激光雨');
+INSERT INTO `say` VALUES ('22', '\r\n青春献给小酒桌，醉生梦死就是喝。');
+INSERT INTO `say` VALUES ('23', '\r\n你是我新鲜感褪去后仅存的心动。');
+INSERT INTO `say` VALUES ('24', '\r\n相爱是山长水阔，最后是你。');
+INSERT INTO `say` VALUES ('25', '\r\n社会没有将与帅，只有实力这一块。');
+INSERT INTO `say` VALUES ('26', '\r\n跨过星河迈过月亮去迎接更好的自己。');
+INSERT INTO `say` VALUES ('27', '\r\n喜欢你在我身后，一回头就是阳光笑容。');
+INSERT INTO `say` VALUES ('28', '\r\n别当待宰羊，做最野的狼。');
+INSERT INTO `say` VALUES ('29', '\r\n你走你的路，他提他的裤，你有你的背景，我有我的故事，不是很硬，但你别碰！');
+INSERT INTO `say` VALUES ('30', '\r\n争强好胜有一天会让你后悔，因为淹死的都是会水的。');
+INSERT INTO `say` VALUES ('31', '\r\n你强归你强 我做我的王');
+INSERT INTO `say` VALUES ('32', '\r\n你是我心安的理由，也是我心慌的源头。');
+INSERT INTO `say` VALUES ('33', '\r\n莫笑穷人穿破衣虎落平阳被犬欺 冷面的社会有心机 硬着头皮笑嘻嘻 没什么怕与不怕 因为家中有老妈和老爸 不触碰我第底线 我可以笑着给你台阶下');
+INSERT INTO `say` VALUES ('34', '\r\n天热脾气燥，我不微笑你别闹。');
+INSERT INTO `say` VALUES ('35', '\r\n点头哈腰是为了学会尊敬，挺直腰板是为了发号施令。');
+INSERT INTO `say` VALUES ('36', '\r\n对你微笑，纯属礼貌，给脸不要，我看你无可救药。');
+INSERT INTO `say` VALUES ('37', '\r\n社会没有遮天树 只有一物降一物');
+INSERT INTO `say` VALUES ('38', '\r\n你对于我而言，如山间清风，如古城暖阳。');
+INSERT INTO `say` VALUES ('39', '\r\n从今以后不谈地久天长，单刀赴会做气质流氓。');
+INSERT INTO `say` VALUES ('40', '\r\n我陪你闹，你就得笑。我给你脸，你就得要。');
+INSERT INTO `say` VALUES ('41', '\r\n我拽归我拽 你拽就拜拜');
+INSERT INTO `say` VALUES ('42', '\r\n放眼过去全是货，老妹儿你想跟谁过');
+INSERT INTO `say` VALUES ('43', '\r\n你一定也是某个人翘首以盼的惊喜。');
+INSERT INTO `say` VALUES ('44', '\r\n希望世界是你心爱的模样。');
+INSERT INTO `say` VALUES ('45', '\r\n你与晚霞同样温暖。');
+INSERT INTO `say` VALUES ('46', '\r\n辉煌时刻人人有，别拿一刻当永久。');
+INSERT INTO `say` VALUES ('47', '\r\n环游遍了整个星系，找不到比你更亮的星星。');
+INSERT INTO `say` VALUES ('48', '\r\n天要下雨娘要嫁人，都是老百姓你给我装什么社会人？');
+INSERT INTO `say` VALUES ('49', '\r\n别爱我没结果，除非花手摇过我。');
+INSERT INTO `say` VALUES ('50', '\r\n父母教会长大，德义方走天下，年轻不能认输，说未来还得付出。记住一句话铁子，玩就玩个利落干脆，做就做他个问心无愧。');
+INSERT INTO `say` VALUES ('51', '\r\n茶倒半盏，酒得到满，先拿筷子在拿碗。');
+INSERT INTO `say` VALUES ('52', '\r\n愿天上的每一个流星，都为你而闪耀天际。');
+INSERT INTO `say` VALUES ('53', '\r\n明争暗斗丢了本性，以卵击石你不够硬，在我面前发号施令，年纪轻轻你就一身毛病。');
+INSERT INTO `say` VALUES ('54', '\r\n头可断，血可留，老子可遇不可求。');
+INSERT INTO `say` VALUES ('55', '\r\n纵然这世间千般好，但唯你最珍贵。');
+INSERT INTO `say` VALUES ('56', '\r\n社会让你呼风唤雨，是我杠杠配不上你');
+INSERT INTO `say` VALUES ('57', '\r\n我见过最蓝的天是初遇你的那天。');
+INSERT INTO `say` VALUES ('58', '\r\n变心的爱人就像狗，谁有兴趣谁牵走');
+INSERT INTO `say` VALUES ('59', '\r\n余光是你，余生也是你。');
+INSERT INTO `say` VALUES ('60', '\r\n水浅王八多，遍地是大哥，不是社会人，别唠社会嗑。');
+INSERT INTO `say` VALUES ('61', '\r\n小伙狂不狂，必须来根芙蓉王。');
+INSERT INTO `say` VALUES ('62', '\r\n被狮子保护过的女人，怎么会看上野狗呢。');
+INSERT INTO `say` VALUES ('63', '\r\n一生最奢侈的事，就是途中与你相遇，然后相濡以沫。');
+INSERT INTO `say` VALUES ('64', '\r\n想你时，漫天星星都是我注视你的眼睛。');
+INSERT INTO `say` VALUES ('65', '\r\n我看过春风十里，见过夏至未至，试过秋光潋滟，爱过冬日暖阳，全都抵不过你一句，我想见你。');
+INSERT INTO `say` VALUES ('66', '\r\n经典语录我肩上扛，少在我面前猖狂。');
+INSERT INTO `say` VALUES ('67', '\r\n我希望你为我而来。');
+INSERT INTO `say` VALUES ('68', '\r\n社会大路一条条，社会歌就得配社会摇。');
+INSERT INTO `say` VALUES ('69', '\r\n愿你忠于自己，活的认真，笑的放肆。');
+INSERT INTO `say` VALUES ('70', '\r\n小的时候我那玩具当朋友，长大后朋友拿我当玩具。');
+INSERT INTO `say` VALUES ('71', '\r\n你是好人我不配，忘了我吧下一位。');
+INSERT INTO `say` VALUES ('72', '\r\n朋友，不要和我硬碰硬，我受的是伤，你丢的是命。');
+INSERT INTO `say` VALUES ('73', '\r\n你说你没喝醉，却摇摇晃晃往后退。');
+INSERT INTO `say` VALUES ('74', '\r\n昼夜平分，暖意生。');
+INSERT INTO `say` VALUES ('75', '\r\n你别找事你别狂，社会的路还很长。');
+INSERT INTO `say` VALUES ('76', '\r\n抽烟只抽炫赫门，一生只爱一个人');
+INSERT INTO `say` VALUES ('77', '\r\n人海冷漠，你是人间炽热。');
+INSERT INTO `say` VALUES ('78', '\r\n关公骑马战千里，你要排面我给你');
+INSERT INTO `say` VALUES ('79', '\r\n你与星光同样浪漫。');
+INSERT INTO `say` VALUES ('80', '\r\n关公在前人在后，小伙注定精神路。');
+INSERT INTO `say` VALUES ('81', '\r\n蝙蝠再飞不是鸟，新鞋再好他不跟脚。');
+INSERT INTO `say` VALUES ('82', '\r\n我希望你是为我而来。');
+INSERT INTO `say` VALUES ('83', '\r\n你别当待宰的羔羊，你要做就做最野的狼！');
+INSERT INTO `say` VALUES ('84', '\r\n有一句话叫做过于高调死的太早，低调一点，哈，养生。');
+INSERT INTO `say` VALUES ('85', '\r\n裤衩天天扔，小曲天天哼。');
+INSERT INTO `say` VALUES ('86', '\r\n激光雨你没看够，小曲儿属实太难受。');
+INSERT INTO `say` VALUES ('87', '\r\n如果你要飞，伤痛我来背，别问我是谁，我是夜来非');
+INSERT INTO `say` VALUES ('88', '\r\n月光不抱你，时光摧毁你，但是我爱你。');
+INSERT INTO `say` VALUES ('89', '\r\n只怪自我太年轻，是人是狗没分清');
+INSERT INTO `say` VALUES ('90', '\r\n我爱你，由表及里。');
+INSERT INTO `say` VALUES ('91', '\r\n天生一身傲骨，你别在你寒哥面前摆谱。');
+INSERT INTO `say` VALUES ('92', '\r\n路还长，别太狂，不定以后谁辉煌。');
+INSERT INTO `say` VALUES ('93', '\r\n万般皆是命，半点不由人，你看我还有几分像从前。');
+INSERT INTO `say` VALUES ('94', '\r\n百因必有果,漂亮妹妹属于我。');
+INSERT INTO `say` VALUES ('95', '\r\n悟空大闹南天门，扳倒各路社会人。 ');
+INSERT INTO `say` VALUES ('96', '\r\n名花虽有主，我来松松土。');
+INSERT INTO `say` VALUES ('97', '\r\n城南花已开，愿君永常在。');
+INSERT INTO `say` VALUES ('98', '\r\n我自明月，向星空，向宇宙，向你。');
+INSERT INTO `say` VALUES ('99', '\r\n你可以打我脸，也可以叫我滚远点，但也别忘了大哥从良前的阴险。');
+INSERT INTO `say` VALUES ('100', '\r\n不与你纠缠不代表我害怕，因为我家中有一个老妈和老爸，你要理解这句话，不叫地主不代表没有炸');
+INSERT INTO `say` VALUES ('101', '\r\n日落西山你不陪，东山再起你是谁。');
+INSERT INTO `say` VALUES ('102', '\r\n交友别摆鸿门宴，社会玩的是排面');
+INSERT INTO `say` VALUES ('103', '\r\n人情世故那么难，让我的心那么寒 ，做人分清眉眼高低 ，耗子没事别舔猫b');
+INSERT INTO `say` VALUES ('104', '\r\n山是山，河是河，动我大哥就是嗑');
+INSERT INTO `say` VALUES ('105', '\r\n下象棋靠的是将，拜鬼神靠的是香，朋友关系处的是帮与不帮，恋人爱情靠的是责任与担当。');
+INSERT INTO `say` VALUES ('106', '\r\n我的社会摇摇到如今，最拿人心，请问你，那什么和我拼？');
+INSERT INTO `say` VALUES ('107', '\r\n你是所有的心跳和欢呼雀跃。');
+INSERT INTO `say` VALUES ('108', '\r\n我本暗淡， 是你给了我漫天星光。');
+INSERT INTO `say` VALUES ('109', '\r\n古有英雄百座坟，怕死绝非社会人');
+INSERT INTO `say` VALUES ('110', '\r\n做什么大哥的女人，要做就做大哥');
+INSERT INTO `say` VALUES ('111', '\r\n我傲？我必须傲，我的傲气不允许我低调。');
+INSERT INTO `say` VALUES ('112', '\r\n不会烧香得罪神，不会说话得罪人。');
+INSERT INTO `say` VALUES ('113', '\r\n生死在天，富贵由命，如今社会就是看谁钱多看谁♂硬，你的寒王，没毛病。');
+INSERT INTO `say` VALUES ('114', '\r\n人就算是极度也能感受到一丝属于自己的温暖。');
+INSERT INTO `say` VALUES ('115', '\r\n我这野马不识归途，但是你这小人，我必须铲除。');
+INSERT INTO `say` VALUES ('116', '\r\n有钱把事办好，没钱把人做好。');
+INSERT INTO `say` VALUES ('117', '\r\n小伙子气势猛如狼，一腔热血拍胸膛经典语录我肩上抗，少在我面前猖狂。');
+INSERT INTO `say` VALUES ('118', '\r\n星光坠入你眼里，你落进我心里。');
+INSERT INTO `say` VALUES ('119', '\r\n我余光中都是你。');
+INSERT INTO `say` VALUES ('120', '\r\n那么耀眼的你，是我永远不会言说的怦然心动～');
+INSERT INTO `say` VALUES ('121', '\r\n踏遍星河，独爱你这颗星。');
+INSERT INTO `say` VALUES ('122', '\r\n红牛雪碧牛栏山，黄泉碧落不孤单。');
+INSERT INTO `say` VALUES ('123', '\r\n希望你也为了我，偷偷地笑着。');
+INSERT INTO `say` VALUES ('124', '\r\n昔日猛虎下山坡，归来依旧是大哥。');
+INSERT INTO `say` VALUES ('125', '\r\n盛意以江河，江河不及你。');
+INSERT INTO `say` VALUES ('126', '\r\n女人就要宠越宠越有种，男人不能惯越惯越混蛋。');
+INSERT INTO `say` VALUES ('127', '\r\n皓月清凉，你是人间曙光。');
+INSERT INTO `say` VALUES ('128', '\r\n我喜欢你，像鲸沉于海底温柔呼吸，痴极嗔极。');
+INSERT INTO `say` VALUES ('129', '\r\n得不到就是得不到，别总说你不想要。');
+INSERT INTO `say` VALUES ('130', '\r\n不要和我比过去，我小的时候比你还淘气。');
+INSERT INTO `say` VALUES ('131', '\r\n可能时间刚好，你眼角带笑。');
+INSERT INTO `say` VALUES ('132', '\r\n遇见你花光了我所有的运气。');
+INSERT INTO `say` VALUES ('133', '\r\n宇宙是一场薄情的幻觉，而我只愿为你多费唇舌。');
+INSERT INTO `say` VALUES ('134', '\r\n想你，成了我的条件反射。');
+INSERT INTO `say` VALUES ('135', '\r\n社会有型哥有样，但哥不是你对象。');
+INSERT INTO `say` VALUES ('136', '\r\n先穿袜子再穿鞋，先当孙子再当爷。');
+INSERT INTO `say` VALUES ('137', '\r\n你是那个让我丢械弃甲、溃退千里的人。');
+INSERT INTO `say` VALUES ('138', '\r\n精神小伙社会话，没有语言不敢跟你骂');
+INSERT INTO `say` VALUES ('139', '\r\n睡不着，可能被子太轻压不住想你的心。');
+INSERT INTO `say` VALUES ('140', '\r\n脸有沧桑，不惧悲伤，我本坏蛋，无限嚣张。');
+INSERT INTO `say` VALUES ('141', '\r\n希望压倒我的不是生活而是你。');
+INSERT INTO `say` VALUES ('142', '\r\n现在的小伙装社会 小姑娘她装太妹 喝点酒她如痴如醉 你给我装鸡毛黑社会');
+INSERT INTO `say` VALUES ('143', '\r\n你不同，只有你完全符合我做的每一个梦。');
+INSERT INTO `say` VALUES ('144', '\r\n仰脖朝天唠嗨嗑，都说自己是大哥。');
+INSERT INTO `say` VALUES ('145', '\r\n我喜欢你，像星辰奔波亿万年黑夜，不诉怨语。');
+INSERT INTO `say` VALUES ('146', '\r\n做事分清眉眼高低，耗子没事儿别舔猫逼');
+INSERT INTO `say` VALUES ('147', '\r\n天黑路滑，月黑风高，没有能耐，别他妈叫嚣。');
+INSERT INTO `say` VALUES ('148', '\r\n大喜大悲看清自己,大起大落看清朋友');
+INSERT INTO `say` VALUES ('149', '\r\n喊疼的不一定是处女，微笑的一定是婊子.');
+INSERT INTO `say` VALUES ('150', '\r\n做人你别太过，做事你别犯错，人生难免起起落落，但是你还得自己把握，纵使生活万般苦涩，咱们也得笑着走过。');
+INSERT INTO `say` VALUES ('151', '\r\n天在下雨地下滑，自己跌倒自己爬。');
+INSERT INTO `say` VALUES ('152', '\r\n你要不服咱就战，两横一竖就是干。');
+INSERT INTO `say` VALUES ('153', '\r\n愿此去繁花似锦，再相逢依然如故。');
+INSERT INTO `say` VALUES ('154', '\r\n你若折我姐妹翅膀 我定废你整个天堂。');
+INSERT INTO `say` VALUES ('155', '\r\n灯红酒绿惹人醉，大哥带你混社会。');
+INSERT INTO `say` VALUES ('156', '\r\n城南城北一条街，打听打听谁是爹。');
+INSERT INTO `say` VALUES ('157', '\r\n我能把你捧上众人之巅，也能让你坠入万丈深渊，吃水都他妈能忘了挖井的人，我又何必再与你纠缠?');
+INSERT INTO `say` VALUES ('158', '\r\n小孩子才分对错，成年人只看利弊。');
+INSERT INTO `say` VALUES ('159', '\r\n你是我的乍见之欢，你是我的眼神所向，你是我的温柔的归宿，你是我的嘴角扬起的理由。');
+INSERT INTO `say` VALUES ('160', '\r\n下象棋靠的是将，拜鬼神靠的是香。');
+INSERT INTO `say` VALUES ('161', '\r\n做人呢是该傲，给你脸呢你得要。');
+INSERT INTO `say` VALUES ('162', '\r\n昨天的太阳永远晒不干今天的衣服，别总提起昨天的自己，你要理解今非昔比。');
+INSERT INTO `say` VALUES ('163', '\r\n踏马关公身上纹，掌声送给社会人。');
+INSERT INTO `say` VALUES ('164', '\r\n社会树下社会坟，来生还做社会人。');
+INSERT INTO `say` VALUES ('165', '\r\n社会让你呼风唤雨，但我寒王真看不上你.');
+INSERT INTO `say` VALUES ('166', '\r\n拜过把子发过誓,这顿咱先AA制。');
+INSERT INTO `say` VALUES ('167', '\r\n兄弟为手足，女人为衣服。谁动我手足，我扒谁衣服。');
+INSERT INTO `say` VALUES ('168', '\r\n我是你的碳酸饮料，一见到你就冒泡。');
+INSERT INTO `say` VALUES ('169', '\r\n我不指望谁能在我困难的时候雪中送炭，你的寒王我只想告诉你们，少做恶事多行善。');
+INSERT INTO `say` VALUES ('170', '\r\n铁子，刀不锋利马太瘦，我还不想和你斗。');
+INSERT INTO `say` VALUES ('171', '\r\n男人，你可以色，但你要扛着色上的一把刀');
+INSERT INTO `say` VALUES ('172', '\r\n天上下雨地上滑，自己跌倒自己爬。');
+INSERT INTO `say` VALUES ('173', '\r\n社会道路都在走，做人做事别太狗。');
+INSERT INTO `say` VALUES ('174', '\r\n大金链子小金表，一天三顿小烧烤。');
+INSERT INTO `say` VALUES ('175', '\r\n和你的一点一滴，我都想用喜欢填满。');
+INSERT INTO `say` VALUES ('176', '\r\n在这个社会上敢怂才是英雄，不怂头破血流，猛男记住一句话，时势造英雄。');
+INSERT INTO `say` VALUES ('177', '\r\n世界那么乱，你装纯给谁看。');
+INSERT INTO `say` VALUES ('178', '\r\n青山一片云雾，心安即归处。');
+INSERT INTO `say` VALUES ('179', '\r\n网络上你重拳出击，现实里你刚上初一。');
+INSERT INTO `say` VALUES ('180', '\r\n生死在天，富贵由命，如今社会就是看谁钱多看谁硬，你的寒王，没毛病。');
+INSERT INTO `say` VALUES ('181', '\r\n我年轻需要你指点，但不需要你指指点点。');
+INSERT INTO `say` VALUES ('182', '\r\n口水不吐就是痰，教人教透就是难。');
+INSERT INTO `say` VALUES ('183', '\r\n老酒新茶，都要与你共饮。');
+INSERT INTO `say` VALUES ('184', '\r\n想把一年四季的美好都赠予你。');
+INSERT INTO `say` VALUES ('185', '\r\n花以香为证，人以富为首.');
+INSERT INTO `say` VALUES ('186', '\r\n从此以后不谈地久天长，单刀赴会做气质流氓。');
+INSERT INTO `say` VALUES ('187', '\r\n手握枪剑指四方，小伙属实很嚣张。');
+INSERT INTO `say` VALUES ('188', '\r\n吻最爱的姑娘，做最野的狼');
+INSERT INTO `say` VALUES ('189', '\r\n东方不亮西方亮，你爹啥样我啥样。');
+INSERT INTO `say` VALUES ('190', '\r\n山多狐狸野，都是社会姐。');
+INSERT INTO `say` VALUES ('191', '\r\n靠山山会倒，靠人人会跑，只有自己最可靠');
+INSERT INTO `say` VALUES ('192', '\r\n社会道路都再走，做人做事别做狗，扎枪搞把公关战，两横一竖就是干。');
+INSERT INTO `say` VALUES ('193', '\r\n我喜欢你，穷极所思，可赴千里。');
+INSERT INTO `say` VALUES ('194', '\r\n外面风大，和我回家。');
+INSERT INTO `say` VALUES ('195', '\r\n记住寒王一句话，男人，高冷。');
+INSERT INTO `say` VALUES ('196', '\r\n装逼让你心打怵，点你的话你自己悟。');
+INSERT INTO `say` VALUES ('197', '\r\n精神小伙不一般，气质能顶半边天');
+INSERT INTO `say` VALUES ('198', '\r\n泪水打湿豆豆鞋，夹上皮包还是爷。');
+INSERT INTO `say` VALUES ('199', '\r\n四个大字：你滴寒王！');
+INSERT INTO `say` VALUES ('200', '\r\n狮子不和狗玩，这是规矩。');
+INSERT INTO `say` VALUES ('201', '\r\n真心不如红钞票，感情只为性需要！');
+INSERT INTO `say` VALUES ('202', '\r\n今天你踩我头上 明天我站你坟上');
+INSERT INTO `say` VALUES ('203', '\r\n你就是条难养的狗，谁对你好你就跟谁走，我不是你妈，给不了你想要的天下。');
+INSERT INTO `say` VALUES ('204', '\r\n头脑社会，金钱拿人。你可以很贵，也可以免费。');
+INSERT INTO `say` VALUES ('205', '\r\n不要在网络上比比赖赖，不服现实碰一碰。');
+INSERT INTO `say` VALUES ('206', '\r\n今朝有酒今朝醉，别是个女人你就想睡。');
+INSERT INTO `say` VALUES ('207', '\r\n是猫都有七分懒，是人都有七分胆。铁子，好好说话，别跟我喊。');
+INSERT INTO `say` VALUES ('208', '\r\n我脾气好是因为我装的，我暴躁起来不要你的命。');
+INSERT INTO `say` VALUES ('209', '\r\n别把辉煌记在心间，别拿昨天挂在嘴边。');
+INSERT INTO `say` VALUES ('210', '\r\n天生一副傲骨，你别在我面前摆谱。喷我呢，你得想清楚，你拿什么，和我赌？');
+INSERT INTO `say` VALUES ('211', '\r\n手拿扎枪烽火令，小伙注定社会命。');
+INSERT INTO `say` VALUES ('212', '\r\n鬼饿敢拦路勾魂，虎饿敢拦路吃人，鱼饿他敢跳龙门 这人饿，丧尽人伦。');
+INSERT INTO `say` VALUES ('213', '\r\n小皮鞋配牛仔裤，俩腿一迈，流氓步！');
+INSERT INTO `say` VALUES ('214', '\r\n众人平庸，你是人间星光。');
+INSERT INTO `say` VALUES ('215', '\r\n人间正道是沧桑，活的不要太嚣张');
+INSERT INTO `say` VALUES ('216', '\r\n水太深风太大，没有实力少说话。');
+INSERT INTO `say` VALUES ('217', '\r\n先擦鼻涕后提裤，从此走上社会路。');
+INSERT INTO `say` VALUES ('218', '\r\n我不懂年少轻狂，只知道胜者为王。');
+INSERT INTO `say` VALUES ('219', '\r\n手持关公戰，不服呢咱就干，中华儿女千千万，sei怂sei四王八蛋');
+INSERT INTO `say` VALUES ('220', '\r\n不是路不平，而是你不行。');
+INSERT INTO `say` VALUES ('221', '\r\n万丈高楼平地起，辉煌只能靠自己。');
+INSERT INTO `say` VALUES ('222', '\r\n精神小伙战四方，兜里没钱还挺装。');
+INSERT INTO `say` VALUES ('223', '\r\n金鸡刚入凤凰窝 不知谁才是大哥。');
+INSERT INTO `say` VALUES ('224', '\r\n十年兴败谁知晓，三穷三富过到老，什么叫好，什么叫不好，没牌面的小弟弟往后稍稍。');
+INSERT INTO `say` VALUES ('225', '\r\n天真灿烂，尘世游慢。');
+INSERT INTO `say` VALUES ('226', '\r\n与其让别人耽误你，那还是我来吧。');
+INSERT INTO `say` VALUES ('227', '\r\n你的所有浪漫我都想参与。');
+INSERT INTO `say` VALUES ('228', '\r\n手握扎枪小卡黄，山东小伙像头狼。');
